c6e52f0c80e57c0462e58305af06844ab641de72
Carlos Linares López <carlos.linares@uc3m.es>
2020-04-09 22:52:11 +0200

    I just realized __init__.py was not part of the changeset ... added

---
+++ spsbot/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+# Description: initialization file
+# -----------------------------------------------------------------------------
+#
+# Login   <carlos.linares@uc3m.es>
+#
+
+"""
+spsbot init file
+"""
+
+
+# Local Variables:
+# mode:python
+# fill-column:80
+# End:
